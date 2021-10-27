d71547f5331bcb3ef6a19d511aa1608f4a2a2268
Masoud <mas.sam1368@gmail.com>
2021-10-27 12:24:02 +0330

    add code to footer of admin page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -146,7 +146,7 @@
                 <footer class="sticky-footer bg-white">
                     <div class="container my-auto">
                         <div class="copyright text-center my-auto">
-                            <span>Copyright &copy; Website Management <script>document.write(new Date().getFullYear())</script></span>
+                            <span>Copyright &copy; <script>document.write(window.location.host)</script> <script>document.write(new Date().getFullYear())</script></span>
                         </div>
                     </div>
                 </footer>
