c3f4e62b68b7e1aa86a3ed47b3f4e8242ea552e7
Masoud <mas.sam1368@gmail.com>
2021-12-21 13:59:03 +0330

    add languages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -275,7 +275,49 @@
                   <p>{{ $education->education_description }}</p>
                 </div>
               @endforeach
+
+              <h3 class="resume-title">Languages</h3>
+              
+
+              <div id="skills" class="skills ">
+               
+          
+          
+                  <div class="row skills-content">
+          
+                     <div class="col-lg-7" data-aos="fade-up">
+                        <div class="progress">
+                          <span class="skill">English<i class="val">90</i></span>
+                          <div class="progress-bar-wrap">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+                        </div>
+                      </div>
+                     <div class="col-lg-7" data-aos="fade-up">
+                        <div class="progress">
+                          <span class="skill">Deutsch<i class="val">80</i></span>
+                          <div class="progress-bar-wrap">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+                        </div>
+                      </div>
+                     <div class="col-lg-7" data-aos="fade-up">
+                        <div class="progress">
+                          <span class="skill">Persian(Native) <i class="val">100</i></span>
+                          <div class="progress-bar-wrap">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+                        </div>
+                      </div>
+                   
+            
+                </div>
+              </div>
+             
+             
             </div>
+
+            
           @endif        
         </div>
       </div>
