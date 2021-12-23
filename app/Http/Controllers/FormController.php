0df54176c668c047d18d52214f19dfe66723fd2c
Masoud <mas.sam1368@gmail.com>
2021-12-23 10:35:14 +0330

    add send mail after get message'

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -3,13 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Models\Message;
+use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class FormController extends Controller
 {
     public function getContactForm(Request $request)
     {
+        $data = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'subject' => $request->subject,
+            'text' => $request->text
+        ];
+
+        Mail::send('mail', $data, function($message) {
+            $user = User::all()->first();
+
+            $message->to($user->email)->subject
+                ('Got new message');
+            $message->from(env('MAIL_FROM_ADDRESS'));
+        });
+
         try {
            Message::create([
                 'name' => $request->name,
